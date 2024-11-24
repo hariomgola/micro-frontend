eabff1f8686194cac6f19c7672bd47d41b3c0768
hariom <hariom987316@gmail.com>
2024-11-25 01:20:09 +0530

    [Adding port address]

--- data/notes.js
+++ data/notes.js
@@ -1,6 +1,6 @@
 const data = () => `
  - ng new micro-frontend --createApplication="false"
- - ng g application host-app
+ - ng g application host-app - 4200 / 4300 / 4400
  - npm i webpack webpack-cli --save-dev
  - ng add @angular-architects/module-federation --project host-app --port 4200
 `;
